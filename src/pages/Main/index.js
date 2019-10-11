a328a8c9816587be3bec1e8f5cb92814a699d12d
João Pedro da Cruz Melo <jopcmelo@gmail.com>
2019-10-11 20:46:42 -0300

    feat: Add Card component

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,37 @@
 import React from 'react'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import Header from '../../components/Header'
 import Tabs from '../../components/Tabs'
+import Menu from '../../components/Menu'
 
-import { Container } from './styles'
+import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from './styles'
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Menu />
+
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 197.611,23</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 20,00 recebida de Diego Shell Fernandes hoje às 06:02h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   )

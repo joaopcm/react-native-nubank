169221bebb1ad7ccc736151216590bb10a722e56
João Pedro da Cruz Melo <jopcmelo@gmail.com>
2019-10-11 22:17:05 -0300

    feat: Add animations

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -5,12 +5,17 @@ import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import { Container, Code, Nav, NavItem, NavText, SignOutButton, SignOutButtonText } from './styles'
 
-export default function Menu() {
+export default function Menu({ translateY }) {
   return (
-    <Container>
+    <Container style={{
+      opacity: translateY.interpolate({
+        inputRange: [0, 150],
+        outputRange: [0, 1]
+      })
+    }}>
       <Code>
         <QRCode
-          value="http://awesome.link.qr"
+          value="https://github.com/jopcmelo"
           size={80}
           gfColor="#fff"
           bgColor="#8b10ae"
